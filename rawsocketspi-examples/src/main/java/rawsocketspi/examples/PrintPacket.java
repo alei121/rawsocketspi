6817047d30304bb9b981b2b5d809f3359403fab5
Alan Lei <alanlei@gmail.com>
2014-11-23 22:41:45 -0800

    Eliminate ByteHelper

--- rawsocketspi-examples/src/main/java/rawsocketspi/examples/PrintPacket.java
+++ rawsocketspi-examples/src/main/java/rawsocketspi/examples/PrintPacket.java
@@ -1,36 +1,49 @@
 package rawsocketspi.examples;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.Formatter;
 
 import rawsocketspi.RSSelectorProvider;
 import rawsocketspi.RSSocketAddress;
 
 public class PrintPacket {
+	private static String bbToString(ByteBuffer bb, String divider, int count) {
+		StringBuilder sb = new StringBuilder();
+		Formatter formatter = new Formatter(sb);
+		for (int i = 0; i < count; i++) {
+			if (i < (count - 1)) {
+				formatter.format("%02X" + divider, bb.get() & 0xFF);
+			} else {
+				formatter.format("%02X", bb.get() & 0xFF);
+			}
+		}
+		formatter.close();
+		return sb.toString();
+	}
 
 	public static void main(String[] args) throws Exception {
-		RSSocketAddress address = new RSSocketAddress("eth1");
+		RSSocketAddress address = new RSSocketAddress(args[0]);
 		
 		RSSelectorProvider provider = new RSSelectorProvider();
 		DatagramChannel channel = provider.openDatagramChannel();
-		
 		channel.bind(address);
 
 		ByteBuffer bb = ByteBuffer.allocateDirect(2048);
-
-		try {
-			while (true) {
-				bb.clear();
-				channel.read(bb);
-				bb.flip();
-				String s = ByteHelper.toStringTrimmed(bb);
-				System.out.println("bb=" + s);
+		while (true) {
+			bb.clear();
+			channel.read(bb);
+			bb.flip();
+			System.out.println("dstMac=" + bbToString(bb, ":", 6) + " srcMac="
+					+ bbToString(bb, ":", 6));
+			int count = bb.remaining();
+			String trail = "";
+			if (count > 20) {
+				count = 20;
+				trail = "...";
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("  content=" + bbToString(bb, " ", count)
+					+ trail);
 		}
-
-		channel.disconnect();
 	}
 }
